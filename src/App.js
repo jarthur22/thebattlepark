19b96fb5cc102b4a3bffff21d6814826521d5210
jakearthur22 <harmonicadude22@gmail.com>
2019-11-20 15:42:57 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './components/Home';
+import Youtubers from './components/youtubers/Youtubers';
+
+class App extends Component{
+
+  getStyle = () => {
+    return {
+      backgroundColor: '#111111',
+      color: 'white',
+      fontFamily: 'Arial,Helvetica,sans-serif',
+      height: '100%',
+      overflow:'auto'
+    }
+  }
+
+  render(){
+    return (
+      <Router>
+        <div className="App" style={this.getStyle()}>
+            <Header/>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/youtubers" component={Youtubers}/>
+            </Switch>
+            <Footer/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer style={footerStyle}>
+            <p>Copyright © 2019 The Battle Park- All Rights Reserved.</p>
+        </footer>
+    )
+}
+
+const footerStyle = {
+    display: 'block',
+    textAlign: 'center',
+    padding: '15px',
+    bottom: '0',
+    borderTop: '2px solid grey'
+}
+
+export default Footer;
