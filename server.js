2f13de89c9849724eb3da513e30da2753817fda2
Jake Arthur <jarthur.jmj@gmail.com>
2019-11-21 10:56:57 -0500

    Added scripts for heroku build

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://bpapp:bpapp123@cluster0-kxab2.mongodb.net/test?retryWrites=true&w=majority';
-
-const app = express();
+const path = require();
+const app = express('path');
 
 //connect to mongoDB
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true})
     .then(() => console.log('MongoDB connected..'))
     .catch(err => console.log(err));
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 4000;
 
