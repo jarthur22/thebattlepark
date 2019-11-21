36caa47a46576e0c6307d28a74e367df11f22691
Jake Arthur <jarthur.jmj@gmail.com>
2019-11-21 16:37:15 -0500

    preliminary design for ecelite4 complete

---
+++ client/src/components/ChampItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class ChampItem extends Component {
+    render() {
+        const username = this.props.champ.username;
+        const profile = this.props.champ.profile;
+        const team = this.props.champ.team;
+
+        return (
+            <div className="champ">
+                <div className="profile">
+                    <img src={require(`${profile}`)} alt={username}/>
+                    <br/>
+                    {username}
+                </div>
+                <br/>
+                <div className="team">
+                    {team.map((mon) => (
+                        <div className="mon">
+                            <img src={require("../images/bplogo.png")} alt={mon} style={{width: "40px"}}/>
+                            <br/>
+                            {mon}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ChampItem;
--- client/src/components/EcElite4.js
+++ client/src/components/EcElite4.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import ChampItem from './ChampItem';
+
 
 class EcElite4 extends Component {
+    state = {
+        champs: [
+            {
+                username: "KiengIv",
+                profile: "./youtubers/profiles/kieng.jpg",
+                team:[
+                    "Charizard",
+                    "Venusaur",
+                    "Blastoise",
+                    "Pikachu",
+                    "Snorlax",
+                    "Lapras"
+                ]
+            }
+        ]
+    }
+
     render() {
         return (
-            <div>
+            <div className="ecelite4">
                 <h1 style={headerStyle}>El Cerrito Elite 4</h1>
-                <p>This is the page for the Elite 4</p>
+                <p>The greatest players in the region. Are you prepared to challenge this gauntlet?</p>
+                <br/>
+                <div className="elite4members">
+                    {/* after members are in db, get request and map members */}
+                    <div className="member">
+                        <img src={require("./youtubers/profiles/bestinwest.jpg")} alt="elite 4 member"/><br/>
+                        Bestinwest
+                    </div>
+                    <div className="member">
+                        <img src={require("./youtubers/profiles/bestinwest.jpg")} alt="elite 4 member"/><br/>
+                        Bestinwest
+                    </div>
+                    <div className="member">
+                        <img src={require("./youtubers/profiles/bestinwest.jpg")} alt="elite 4 member"/><br/>
+                        Bestinwest
+                    </div>
+                    <div className="member">
+                        <img src={require("./youtubers/profiles/bestinwest.jpg")} alt="elite 4 member"/><br/>
+                        Bestinwest
+                    </div>
+                </div>
+                <br/>
+                <h2>Hall of Champions</h2>
+                <div className="champs">
+                    {this.state.champs.map((champ) => (
+                        <ChampItem key={champ.username} champ={champ}/>
+                    ))}
+                </div>
             </div>
         )
     }
