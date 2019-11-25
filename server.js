441506e83df14c1d45184023a7e204d2be1de0e5
Jake Arthur <jarthur.jmj@gmail.com>
2019-11-25 16:00:50 -0500

    undo validation

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if(process.env.NODE_ENV === 'production'){
     });
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
