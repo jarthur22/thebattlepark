815e6cdbdb3a778543a8d94236997974a3800f14
Jake Arthur <jarthur.jmj@gmail.com>
2019-11-25 15:18:02 -0500

    bug fix

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://bpapp:bpapp123@cluster0-kxab2.mongodb.net/test?retryWrites=true&w=majority';
 const path = require('path');
-const validationFile = require('./8D90E199F9A50EA20EB5E4EC0CE1F0D0.txt');
 
 const app = express();
 
-app.use("/.well-known/pki-validation/8D90E199F9A50EA20EB5E4EC0CE1F0D0.txt", validationFile);
+
 
 //connect to mongoDB
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true})
@@ -24,6 +23,10 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 const port = process.env.PORT || 4000;
+
+app.get('/.well-known/pki-validation/8D90E199F9A50EA20EB5E4EC0CE1F0D0.txt', (req, res) => {
+    res.sendfile('8D90E199F9A50EA20EB5E4EC0CE1F0D0.txt');
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
