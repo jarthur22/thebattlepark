0b8852c13c469ccb25e39092168dc30e641f01c3
Jake Arthur <jarthur.jmj@gmail.com>
2019-12-10 12:43:55 -0500

    discord redirect

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,7 +5,10 @@ class Login extends Component {
 
     goToDiscordAuth = () => {
         console.log('here')
-        axios.get('/api/discord/login').then(console.log('Redirecting..'));
+        const CLIENT_ID = '562770948764205077';
+        const redirect = encodeURIComponent('http://localhost:4000/api/discord/callback');
+        //axios.get('/api/discord/login').then(console.log('Redirecting..'));
+        window.location.href = `https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`;
     }
 
     render() {
--- routes/api/discord.js
+++ routes/api/discord.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const axios = require('axios');
 
 const app = express();
 
-const CLIENT_ID = '562770948764205077';
+/* const CLIENT_ID = '562770948764205077';
 const CLIENT_SECRET = '-eQrhkMMZS-aEvuzpCJuoQ9z6BMNZdSk';
 const redirect = encodeURIComponent('http://localhost:4000/api/discord/callback');
 
 app.get('/login', (req, res) => {
     console.log("here");
-  res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`);
-});
+    res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`);
+}); */
 
 module.exports = router;
